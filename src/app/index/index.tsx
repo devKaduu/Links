a8ffdafc2c0495b5827962e2d9b1e5c23538ead7
Carlos Eduardo <carlos.oliveira@fiap.com.br>
2025-01-17 02:37:29 -0300

    feat: creating option component

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,10 +1,11 @@
-import { Image, View, TouchableOpacity, FlatList } from "react-native";
+import { Image, View, TouchableOpacity, FlatList, Modal, Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import styles from "./styles";
 import { colors } from "@/styles/colors";
 
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
+import { Option } from "@/components/option";
 
 export default function Index() {
   return (
@@ -15,9 +16,7 @@ export default function Index() {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
-
       <Categories />
-
       <FlatList
         data={["1", "2", "3", "4", "5"]}
         keyExtractor={(item) => item}
@@ -32,6 +31,26 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+
+      <Modal transparent visible>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons name="close" size={20} color={colors.gray[400]} />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.modalLinkName}>Google</Text>
+            <Text style={styles.modalUrl}>https://www.google.com.br/?hl=pt-B</Text>
+
+            <View style={styles.modalFooter}>
+              <Option name="Excluir" icon="delete" variant="secondary" />
+              <Option name="Abrir" icon="language" />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
